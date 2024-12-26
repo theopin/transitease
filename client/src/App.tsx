1436e85cb03acae21b01dfc8da9c9f6cad81d9bc
theopin <theopinto_tg104@hotmail.com>
2024-12-26 22:20:39 +0800

    Add frontend routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,23 @@ import BusDetailsDisplay from './components/bus/service/details/BusDetailsDispla
 import BusServiceRoutes from './components/bus/service/routes/BusServiceRoutes'
 import BusStopDetails from './components/bus/stop/details/BusStopDetails'
 import BusStopArrivals from './components/bus/stop/arrivals/BusStopArrivals'
+import { BrowserRouter, Navigate, Route, Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <h1>Transport API Example</h1>
-      {/* <BusDetailsDisplay /> */}
-      {/* <BusServiceRoutes /> */}
-      <BusStopArrivals></BusStopArrivals>
-      {/* <BusStopDetails></BusStopDetails> */}
+      <BrowserRouter>
+          <Routes>
+            {/* Default route redirects to BusStopArrivals */}
+            <Route path="/" element={<Navigate to="/bus/stops/arrivals" replace />} />
+            <Route path="/bus/details" element={<BusDetailsDisplay />} />
+            <Route path="/bus/routes" element={<BusServiceRoutes />} />
+            <Route path="/bus/stops/arrivals" element={<BusStopArrivals />} />
+            <Route path="/bus/stops" element={<BusStopDetails />} />
+          </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
